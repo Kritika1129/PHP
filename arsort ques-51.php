6ea78bab88382da7403e39acb4ba1b9b0b02f735
KRITIKA <144862402+Kritika1129@users.noreply.github.com>
2025-02-23 00:50:18 +0530

    Update arsort ques-51.php

--- arsort ques-51.php
+++ arsort ques-51.php
@@ -1,17 +1,24 @@
-<html> 
+<!DOCTYPE html>
 <html lang="en"> 
 <head> 
-<title>Sorting PHP Associative Array in 
-Descending Order by Value</title> 
+    <title>Sorting PHP Associative Array in Descending Order by Value</title> 
 </head> 
 <body> 
-<?php 
-// Define array 
-Sage = array("Peter"=>20, "Harry"=>14, 
-// Sorting array by value and print 
-arsort($age); 
-print_r($age); 
-echo"this program is written by kritika erp-067";
+    <?php 
+    // Define array 
+    $age = array("Peter" => 20, "Harry" => 14, "John" => 45, "Clark" => 35);
 
+    // Sorting array by value in descending order
+    arsort($age);
 
-</html> 
+    // Printing sorted array
+    echo "<h3>Sorted Ages in Descending Order:</h3>";
+    foreach ($age as $name => $value) {
+        echo "$name: $value <br>";
+    }
+
+    // Adding the requested message
+    echo "<br>This program is written by KRITIKA ERP-067<br>";
+    ?>
+</body> 
+</html>
