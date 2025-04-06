5f0c3568221432a06b0a19f3d0dcef0981aea21a
KRITIKA <144862402+Kritika1129@users.noreply.github.com>
2025-04-06 11:38:17 +0530

    Add files via upload

---
+++ ques-59 str lower.php
@@ -0,0 +1,14 @@
+<?php
+// Original string
+$text = "Hello PHP, I LOVE Coding!";
+
+// Convert to lowercase
+$lowerText = strtolower($text);
+
+// Display the result
+echo "Original Text: " . $text . "<br>";
+echo "Lowercase Text: " . $lowerText . "<br>";
+
+// Add author line
+echo "This program is written by KRITIKA ERP-067";
+?>
