cbf0bab36d5c806309a2f8f12d0fe990cf6aa0de
KRITIKA <144862402+Kritika1129@users.noreply.github.com>
2025-04-06 11:34:54 +0530

    Add files via upload

---
+++ ques-57 str replace.php
@@ -0,0 +1,13 @@
+<?php
+// Original string
+$text = "Hello world! Welcome to the world of PHP.";
+
+// Replace "world" with "universe"
+$replacedText = str_replace("world", "universe", $text);
+
+// Display the result
+echo $replacedText;
+
+// Add author line
+echo "<br>This program is written by KRITIKA ERP-067";
+?>
