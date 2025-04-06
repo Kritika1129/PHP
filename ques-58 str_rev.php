bcc7974474f1160182e217246aa841c13e9a9c09
KRITIKA <144862402+Kritika1129@users.noreply.github.com>
2025-04-06 11:36:45 +0530

    Add files via upload

---
+++ ques-58 str_rev.php
@@ -0,0 +1,14 @@
+<?php
+// Original string
+$text = "Hello PHP";
+
+// Reverse the string
+$reversedText = strrev($text);
+
+// Display the result
+echo "Original Text: " . $text . "<br>";
+echo "Reversed Text: " . $reversedText . "<br>";
+
+// Add author line
+echo "This program is written by KRITIKA ERP-067";
+?>
