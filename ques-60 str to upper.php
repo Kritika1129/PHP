611f4fcfbaaefd56c02093e453ac0603bb19bc52
KRITIKA <144862402+Kritika1129@users.noreply.github.com>
2025-04-06 11:40:13 +0530

    Add files via upload

---
+++ ques-60 str to upper.php
@@ -0,0 +1,14 @@
+<?php
+// Original string
+$text = "Hello PHP, I love coding!";
+
+// Convert to uppercase
+$upperText = strtoupper($text);
+
+// Display the result
+echo "Original Text: " . $text . "<br>";
+echo "Uppercase Text: " . $upperText . "<br>";
+
+// Add author line
+echo "This program is written by KRITIKA ERP-067";
+?>
